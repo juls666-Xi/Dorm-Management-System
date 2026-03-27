bedfc97c259563e7cfa5be85c9e3b64a1f4a2577
juls666-Xi <sfxcs.juliusjordan@gmail.com>
2026-03-27 06:42:41 +0000

    plublish

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/Dorm-Management-System/",
   plugins: [react()],
   server: {
     proxy: {
